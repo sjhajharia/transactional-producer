2480b16dc288f6f6915d6cc0f73189a0a5825448
Sanskar Jhajharia <sjhajharia@confluent.io>
2024-02-05 11:49:55 +0530

    Adding files

---
+++ src/test/java/com/sjhajharia/TransactionalProducerTest.java
@@ -0,0 +1,13 @@
+package com.sjhajharia;
+
+import org.junit.Test;
+
+import java.io.IOException;
+
+public class TransactionalProducerTest {
+    @Test
+    public void verifyNoExceptionThrown() throws IOException {
+        System.out.println("Heeeee");
+        //TransactionalProducer.main(new String[]{});
+    }
+}
